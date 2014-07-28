72a48f1f1a49670e9dc22ab9575a6df0842638c1
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-07-28 14:52:17 +0300

    ResponseSet building

--- ResponseSet.java
+++ ResponseSet.java
@@ -1,19 +1,38 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class ResponseSet {
 	
 	private ArrayList<Response> responses;
 	
-	private static final int NO_VEG = 0;
-	private static final int VEGETARIAN = 1;
-	private static final int VEGAN = 2;
+	private static final Integer NO_VEG = 0;
+	private static final Integer VEGETARIAN = 1;
+	private static final Integer VEGAN = 2;
+	
+	private static final String RARELY_STRING = "Rarely";
+	private static final String SOMETIMES_STRING = "Sometimes";
+	private static final String OFTEN_STRING = "Often";
+	private static final String MOSTLY_STRING = "Most of the time";
+	
+	private static final Integer RARELY_INT = 0;
+	private static final Integer SOMETIMES_INT = 1;
+	private static final Integer OFTEN_INT = 2;
+	private static final Integer MOSTLY_INT = 3;
+	
+	private static final HashMap<String, Integer> conditionsToInts = new HashMap<String, Integer>();
 	
 	public ResponseSet(String filename) throws Exception {
+		
+		conditionsToInts.put(RARELY_STRING, RARELY_INT);
+		conditionsToInts.put(SOMETIMES_STRING, SOMETIMES_INT);
+		conditionsToInts.put(OFTEN_STRING, OFTEN_INT);
+		conditionsToInts.put(MOSTLY_STRING, MOSTLY_INT);
+		
 		Scanner scanner = new Scanner (new File ("Placeholder for name"));
 		scanner.useDelimiter(",");
-		ArrayList<ArrayList<String>> responses = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> responseStrings = new ArrayList<ArrayList<String>>();
 		ArrayList<String> currentResponse=new ArrayList<String>();
 	
 		while (scanner.hasNext())
@@ -22,13 +41,13 @@ public class ResponseSet {
 					currentResponse.add(scanner.next());
 				}
 				else {
-					responses.add(currentResponse);
+					responseStrings.add(currentResponse);
 					currentResponse = new ArrayList<String>();
 				}
 			}
 
 			for(int i = 0; i < responses.size(); i++) {
-				ArrayList<String> myResponse = responses.get(i);
+				ArrayList<String> myResponse = responseStrings.get(i);
 				Response response = new Response();
 				response.setFastFood(Integer.parseInt(myResponse.get(0)));
 				
@@ -45,6 +64,25 @@ public class ResponseSet {
 				else {
 					throw new Exception("Invalid veg type");
 				}
+				
+				String oilButterString = myResponse.get(2);
+				response.setOilButter(conditionsToInts.get(oilButterString));
+				
+				String eggsString = myResponse.get(3);
+				response.setEggs(conditionsToInts.get(eggsString));
+				
+				String dessertsString = myResponse.get(4);
+				response.setDesserts(conditionsToInts.get(dessertsString));
+				
+				String bloodTypeString = myResponse.get(5);
+				response.setBloodType(bloodTypeString);
+				
+				/* TODO: randomize the Unknown blood types */
+				
+				String name = myResponse.get(6);
+				response.setName(name);
+				
+				responses.add(response);
 			}
 		
 			scanner.close();	
