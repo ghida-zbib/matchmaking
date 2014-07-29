5525a1126172050f0c5296884886b3012b0d96c5
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-07-29 15:09:45 +0300

    split lists, build preferences in runner

--- KidneyRunner.java
+++ KidneyRunner.java
@@ -7,7 +7,27 @@ public class KidneyRunner {
 	private ArrayList<ArrayList<KCIPerson>> split(ArrayList<KCIPerson> people) {
 		// split into half donors, half recipients
 		// (TODO)
-		return new ArrayList<ArrayList<KCIPerson>>();
+		
+		int halfwayPoint = people.size()/2;
+		
+		ArrayList<KCIPerson> donorsList = new ArrayList<KCIPerson>();
+		ArrayList<KCIPerson> recipientsList = new ArrayList<KCIPerson>();
+		
+		for(int i = 0; i < people.size(); i++) {
+			if(i < halfwayPoint) {
+				recipientsList.add(people.get(i));
+			}
+			else {
+				donorsList.add(people.get(i));
+			}
+		}
+		
+		ArrayList<ArrayList<KCIPerson>> lists = new ArrayList<ArrayList<KCIPerson>>();
+		
+		lists.add(donorsList);
+		lists.add(recipientsList);
+		
+		return lists;
 	}
 	
 	public void runFromCSV(String filename) {
@@ -28,6 +48,15 @@ public class KidneyRunner {
 			ArrayList<KCIPerson> donors = splitLists.get(0);
 			ArrayList<KCIPerson> recipients = splitLists.get(1);
 			
+			for(int i = 0; i < donors.size(); i++) {
+				donors.get(i).buildPreferences(recipients);
+			}
+			
+			for(int i = 0; i < recipients.size(); i++) {
+				recipients.get(i).buildPreferences(donors);
+			}
+			
+			// run gale shapley here
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
